417e41595df54d2a7460db01e2a98aa029714ccf
David Benoit <ddavidrb@gmail.com>
2017-02-03 13:27:10 -0500

    add rsound-composer

---
+++ rsound/rsound-composer/composer.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(require "../main.rkt"
+         "composer-base.rkt"
+         "composer-rsound.rkt"
+         "composer-rsound-instrument.rkt")
+
+
+(provide (all-from-out 
+           "composer-base.rkt"
+           "composer-rsound.rkt"
+           "composer-rsound-instrument.rkt"))
